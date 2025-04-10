facff42ffbdfc5235c5d6982be77960877e8efcc
lucynottonson <lucynottonson@icloud.com>
2025-04-09 18:51:11 -0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-chivo-mono)', 'monospace'],  
+      },
+      fontWeight: {
+        100: '100',
+        200: '200',
+        300: '300',
+        400: '400',
+        500: '500',
+        600: '600',
+        700: '700',
+        800: '800',
+        900: '900',
+      },
+    },
+  },
+  plugins: [],
+});
