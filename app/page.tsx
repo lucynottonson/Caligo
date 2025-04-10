af271d39cdc18fd8d211f0a5939ce1c3be503d10
lucynottonson <lucynottonson@icloud.com>
2025-04-09 20:38:02 -0700

    idk why i need this but apparently do

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default async function LoginPage() {
   }
 
   return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
         <ClientLogin />
       </div>
     </div>
